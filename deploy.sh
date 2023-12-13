9f67d9b38dfa4b543c9657012b90e7dfb5cd33b5
lai <39227868+laimumian@users.noreply.github.com>
2023-12-13 18:57:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-docker build -t hellojavasec .  -v /root/Hello_Java_Sec:/app && docker run -itd -p 8888:8888 -v /root/Hello_Java_Sec:/app hellojavasec 
+docker build -t hellojavasec . && docker run -itd -p 8888:8888 -v /root/Hello_Java_Sec:/app hellojavasec 
